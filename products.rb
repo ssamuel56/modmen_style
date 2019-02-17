cdd69846b338a874c5aee3d2258df3fb7dd43c5a
ssamuel56 <samuelkirkendoll88@gmail.com>
2019-02-16 23:37:09 -0500

    Made a class to handle my products so easier to display

--- products.rb
+++ products.rb
@@ -1,5 +1,19 @@
 class Product
 
-  
+  def initialize(name, description, cost, picture)
+    @name = name
+    @description = description
+    @cost = cost 
+    @picture = picture
+  end
+
+  def props
+    return [
+      @name,
+      @description,
+      @cost,
+      @picture
+    ]
+  end
 
 end
